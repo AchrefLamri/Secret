e9719947c04c1005288e1eb03e3a529ffeb5f586
AchrefLamri <achrefkamra@gmail.com>
2021-08-06 19:38:09 +0200

    Add Envieromnet Vars

--- model/user.js
+++ model/user.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 const encrypt = require('mongoose-encryption');
 
@@ -15,8 +16,7 @@ const userSchema = new Schema({
     });
 
 // encryption the password in mongoose 
-let secret = 'Achraf is vergin'
-userSchema.plugin(encrypt,{secret:secret,encryptedFields:['password']});
+userSchema.plugin(encrypt,{secret:process.env.SECRETE,encryptedFields:['password']});
 
 
 const User = mongoose.model('User',userSchema);
