498a2605423fd623295434bed9d484e21c0474c5
AchrefLamri <achrefkamra@gmail.com>
2021-08-10 19:01:00 +0200

    authenticate using passport to add cookies and sessions

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require("mongoose-encryption");
-// Hash method 
-const md5 = require('md5');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+// 
+const  LocalStrategy = require("passport-local")
+const session = require('express-session');
+const passport = require ('passport');
+const passportLocalMongoose = require("passport-local-mongoose");
+
+// // md5 Hash method 
+// const md5 = require('md5');
+// // bcrypt Hash method
+// const bcrypt = require('bcrypt');
+// const saltRounds = 10;//how much Hash  
 
 const User = require('./model/user');
 
@@ -23,6 +29,27 @@ app.set('view engine','ejs');
 // set up the static file
 app.use(express.static('public'));
 
+// set up the session configutation
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+  }));
+
+// set up passport 
+app.use(passport.initialize());//initialize passport
+app.use(passport.session());// use passport to mange our session
+
+// Simplified Passport/Passport-Local Configuration
+
+// passport loca mongo to create a local login strategi 
+passport.use(User.createStrategy()); 
+// set passport to serialize and deserialize User 
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead
+mongoose.set('useCreateIndex', true);
 // connect to DataBase
 const uri = 'mongodb://localhost:27017/UserDB';
 
@@ -38,54 +65,83 @@ app.get('/',(req,res)=>{
 app.get('/login',(req,res)=>{
     res.render('login')
 });
+
+// logout Route
+app.get('/logout',(req,res)=>{
+    req.logout();
+    res.redirect('/')
+});
+
 // register Route
 app.get('/register',(req,res)=>{
     res.render('register')
 });
+// secrets
+app.get('/secrets',(req,res)=>{
+    if(req.isAuthenticated()){
+        res.render('secrets')
+    }else{
+        console.log(req.isAuthenticated())
+        res.redirect('/login')
+    }
+});
 
 // register New user
 app.post('/register',(req,res)=>{
-    
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        // Store hash in your password DB.
-        let    newUser = new User({
-            email:req.body.username,
-            password:hash
-        });
-        
-        newUser.save()
-        .then(()=>{       
-            console.log('don :)')
-            res.render('secrets')
-        })
-        .catch((err)=>console.log(err))
-    });
-    
-  
-})
+
+    User.register(new User({username:req.body.username}),req.body.password,(err,user)=>{
+
+        if(err){
+            console.log(err);
+            res.redirect('/register');
+        }else{
+
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
+            });          
+        }
+    })
+
+});
+
 
 // Login to your account 
 app.post('/login',(req,res)=>{
-    let username = req.body.username,
-        password = req.body.password;
 
-    User.findOne({email:username})
-    .then(foundUser =>{
-        if(foundUser){
-            bcrypt.compare(password, foundUser.password)
-            .then((result)=> {
-                if(result){
-                    res.render('secrets')
-                }else{
-                    res.send('password is not correct try agin :(')
-                }
-            });
-        
+    let user  = new User ({
+        username : req.body.username,
+        password : req.body.password
+    })  
+    req.logIn(user,(err)=>{
+        if(err){
+            console.log(err)
         }else{
-            res.send("Not Found :( plase register this user .")
+            passport.authenticate('local')(req,res,()=>{
+                res.redirect('/secrets')
+            })
         }
     })
-    .catch(err =>console.log(err))
+
+
+    // .then(foundUser =>{
+
+    //     if(foundUser){
+    //         // bcrypt.compare(passwordtext, bcrypt.hash)
+    //         bcrypt.compare(password, foundUser.password)
+    //         .then((result)=> {
+    //             // result true or False 
+    //             if(result){
+    //                 res.render('secrets')
+    //             }else{
+    //                 res.send('password is not correct try agin :(')
+    //             }
+    //         });
+        
+    //     }else{
+    //         res.send("Not Found :( plase register this user .")
+    //     }
+    // })
+    // .catch(err =>console.log(err))
         
 })
 
--- model/user.js
+++ model/user.js
@@ -1,23 +1,20 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const encrypt = require('mongoose-encryption');
-
+const passportLocalMongoose = require ('passport-local-mongoose');
 
 const Schema = mongoose.Schema;
 const userSchema = new Schema({
-          email:{
-              type:String,
-              required:true
-          },
-          password:{
-            type:String,
-            required:true
-        }
+          email:String,
+          password:String
     });
 
 // encryption the password in mongoose 
-userSchema.plugin(encrypt,{secret:process.env.SECRETE,encryptedFields:['password']});
+// userSchema.plugin(encrypt,{secret:process.env.SECRETE,encryptedFields:['password']});
 
+
+// set up user schema to use passport local mongoose as a blugin
+userSchema.plugin(passportLocalMongoose);
 
 const User = mongoose.model('User',userSchema);
 
